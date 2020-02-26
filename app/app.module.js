38722e30a04fc5c310a29262fa750cfd019a961d
BoykovaAS <55656518+BoykovaAS@users.noreply.github.com>
2020-02-26 20:44:33 +0300

    Update app.module.js

--- app/app.module.js
+++ app/app.module.js
@@ -4,6 +4,6 @@ angular.module('designApp', [
   'ngResource'
 ]).config(function ($mdThemingProvider) {
   $mdThemingProvider.theme('default')
-    .primaryPalette('grey')
-    .accentPalette('teal');
+    .primaryPalette('bisque')
+    .accentPalette('bisque');
 });
